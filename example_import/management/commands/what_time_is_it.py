d3a2fbb969389a5353f7c1fc2b9e050a638d92c9
Robert Coniglio <coniglio.roberto82@gmail.com>
2025-05-28 16:19:43 +0200

    BaseCommand options

--- example_import/management/commands/what_time_is_it.py
+++ example_import/management/commands/what_time_is_it.py
@@ -4,6 +4,16 @@ from django.utils import timezone
 class Command(BaseCommand):
     help = 'Displays current time'
 
+    def add_arguments(self, parser):
+        parser.add_argument('total', type=int, help='Print number to be printed')
+        #optional argument
+        parser.add_argument('-p', '--prefix', type=str, help='Define a prefix', )
+
     def handle(self, *args, **kwargs):
-        time = timezone.now().strftime('%X')
-        self.stdout.write("It's now %s" % time)
+        #time = timezone.now().strftime('%X')
+        #self.stdout.write("It's now %s" % time)
+        total = kwargs['total']
+        prefix = kwargs['prefix']
+        print(total)
+        if prefix:
+            print(prefix)

f156c50c77c2c980bb75cf61a86b6cfbe27b1e69
Robert Coniglio <coniglio.roberto82@gmail.com>
2025-06-26 16:00:41 +0200

    query agg e join

--- django_postgis/urls.py
+++ django_postgis/urls.py
@@ -29,7 +29,7 @@ from streets.views import NycStreetGeometryValue
 from streets.views import NycStreetsIntersectMeridian
 from streets.views import NycStreetsNearest
 from streets.views import NycStreetsNearestAll
-from subway_stations.views import NycSubwayStationsListCreateAPIView, NycRoutesFromStations
+from subway_stations.views import NycSubwayStationsListCreateAPIView, NycRoutesFromStations, NycPopulationFromTrainStop
 from subway_stations.views import FindNeighborhoodsInSubway
 from subway_stations.views import NycSubwayGetGeog
 from neighborhoods.views import NycNeighborhoodArea, NycNeighborhoodBoroArea
@@ -57,6 +57,7 @@ urlpatterns = [
     path("subwayStations/",NycSubwayStationsListCreateAPIView.as_view(),name="subway_stations"),
     path("subwayGeom/",NycSubwayGetGeog.as_view(),name="subway_geog"),
     path("subwayRoutes/",NycRoutesFromStations.as_view(),name="subway_routes"),
+    path("subwayPopulationTrainStop/",NycPopulationFromTrainStop.as_view(),name="subway_routes"),
     path("neighborhoodInSubway/",FindNeighborhoodsInSubway.as_view(),name="neighborhood_in_subway"),
     path("neighborhoodTopoElements/", NycSharedTopoElements.as_view(), name="neighborhood_topo_elements"),
     path("neighborhoodTopoElementsView/", NycSharedTopoElementsAsView.as_view(), name="neighborhood_topo_elements_view"),
--- subway_stations/views.py
+++ subway_stations/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from census_blocks.models import NycCensusBlocks
 from subway_stations.models import NycSubwayStations
 from subway_stations.models import NycSubwayStationsSerializer
 from neighborhoods.models import NycNeighborhood
@@ -11,6 +12,7 @@ from neighborhoods.models import NycNeighborhood
 from django.contrib.gis.db.models.functions import Transform
 from django.template import loader
 from django.http import HttpResponse
+from django.db.models import Sum
 from django.db.models import F
 import json
 
@@ -76,6 +78,36 @@ class NycRoutesFromStations(APIView):
         routes = NycSubwayStations.objects.values('routes').distinct()
         print(routes)
         return Response(routes)
+
+class NycPopulationFromTrainStop(APIView):
+    '''
+    SELECT
+      100*SUM(c.popn_white)/SUM(c.popn_total) AS white_pct,
+      100*SUM(c.popn_black)/SUM(c.popn_total) AS black_pct,
+      SUM(popn_total) AS popn_total
+    FROM nyc_census_blocks AS c
+    JOIN nyc_subway_stations AS s
+      ON ST_DWithin(
+        c.geom,
+        s.geom,
+        200
+      )
+    WHERE strpos(s.routes,'A') > 0;
+    '''
+    def get(self, request):
+        subway_stations = NycSubwayStations.objects.filter(routes__contains='A')
+        popn_white_total = 0
+        popn_black_total = 0
+        popn_total_t = 0
+        for s in subway_stations:
+            census_blocks = NycCensusBlocks.objects.filter(geom__dwithin=(s.geom, 200)).aggregate(Sum('popn_total'), Sum('popn_white'), Sum('popn_black'))
+            popn_total_t = popn_total_t + census_blocks["popn_total__sum"]
+            popn_white_total = popn_white_total + census_blocks["popn_white__sum"]
+            popn_black_total = popn_black_total + census_blocks["popn_black__sum"]
+        r_make_up = {"white_pct": 100 * (popn_white_total / popn_total_t),
+                     "black_pct": 100 * (popn_black_total / popn_total_t),
+                     "popn_total": popn_total_t}
+        return Response(r_make_up)
 
 def map_view(request, id):
     subway = NycSubwayStations.objects.annotate(geog=Transform('geom', 4326)).get(gid=id)
